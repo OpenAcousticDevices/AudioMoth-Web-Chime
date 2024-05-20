7ae79d3e5865408c9c38f700826d3ac909ecbdf9
Peter Prince <github@pcprince.co.uk>
2024-05-20 09:53:27 +0100

    Add location

--- scripts/index.js
+++ scripts/index.js
@@ -6,15 +6,24 @@
 
 /* global AudioMothChimeConnector */
 
-let audioMothChimeConnector;
-
-let timeHolder;
-
 let iphoneWarning;
+
+let audioMothChimeConnector;
 
 let timeLabel, timeZoneLabel;
 
+let locationSwitchLabel, locationSwitch;
+let latLabel, lonlabel;
+let waitingLabel;
+
 let chimeButton;
+
+let locationDisabled = false;
+
+let GREY_COLOUR = '#919191';
+let WHITE_COLOUR = 'white';
+
+let lat, lon;
 
 // Keep time UI updated
 
@@ -39,6 +48,24 @@ function updateTime () {
     updateTimeZone();
 
     setTimeout(updateTime, 1000);
+
+}
+
+function showWaitingLabel () {
+
+    waitingLabel.innerText = 'Acquiring current location';
+
+}
+
+function showLocationUnavailableLabel () {
+
+    waitingLabel.innerText = 'Location is not available';
+
+}
+
+function hideLabels () {
+
+    waitingLabel.innerText = '';
 
 }
 
@@ -108,14 +135,52 @@ function updateTimeZone () {
 function handleChime () {
 
     chimeButton.disabled = true;
+    locationSwitch.disabled = true;
 
-    const date = new Date();
+    let date;
 
-    audioMothChimeConnector.playTime(date, () => {
+    if (!locationSwitch.checked) {
 
-        chimeButton.disabled = false;
+        date = new Date();
 
-    });
+        audioMothChimeConnector.playTime(date, undefined, undefined, () => {
+
+            if (!locationDisabled) {
+
+                locationSwitch.disabled = false;
+
+            }
+
+            chimeButton.disabled = false;
+
+        });
+
+    } else {
+
+        date = new Date();
+
+        if (validLocationReceived) {
+
+            audioMothChimeConnector.playTime(date, lat, lon, () => {
+
+                chimeButton.disabled = false;
+                locationSwitch.disabled = false;
+
+            });
+
+        } else {
+
+            if (!locationDisabled) {
+
+                chimeButton.disabled = false;
+
+            }
+
+            disableLocationUI();
+
+        }
+
+    }
 
 }
 
@@ -141,12 +206,116 @@ function checkMobile () {
 
 }
 
-function loadPage () {
+function updatePosition (position) {
 
-    // Start page
+    if (!locationSwitch.checked) {
+
+        return;
+
+    }
+
+    latLabel.style.color = WHITE_COLOUR;
+    lonLabel.style.color = WHITE_COLOUR;
+
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+
+    let latString = Math.abs(lat).toFixed(6);
+    latString += '°';
+    latString += lat >= 0 ? 'N' : 'S';
+
+    let lonString = Math.abs(lon).toFixed(6);
+    lonString += '°';
+    lonString += lon >= 0 ? 'E' : 'W';
+
+    // console.log('New position:', latString, lonString);
+
+    latLabel.innerText = latString;
+    lonLabel.innerText = lonString;
+
+    setTimeout(() => {
+
+        navigator.geolocation.getCurrentPosition((position) => {
+
+            updatePosition(position);
+
+        }, disableLocationUI);
+
+    }, 1000);
+
+}
+
+function disableLocationUI () {
+
+    latLabel.style.color = GREY_COLOUR;
+    lonLabel.style.color = GREY_COLOUR;
+
+    showLocationUnavailableLabel();
+
+    locationSwitch.checked = false;
+    locationSwitch.disabled = true;
+
+    locationSwitchLabel.style.color = GREY_COLOUR;
+
+    locationDisabled = true;
+
+}
+
+function handleLocationToggle () {
+
+    if (locationSwitch.checked) {
+
+        validLocationReceived = false;
+
+        chimeButton.disabled = true;
+
+        setTimeout(() => {
+
+            if (!validLocationReceived && locationSwitch.checked) {
+
+                showWaitingLabel();
+
+            }
+
+        }, 1000);
+
+        navigator.geolocation.getCurrentPosition((position) => {
+
+            validLocationReceived = true;
+
+            hideLabels();
+
+            updatePosition(position);
+
+            chimeButton.disabled = false;
+
+        }, () => {
+
+            disableLocationUI();
+
+            chimeButton.disabled = false;
+
+        });
+
+    } else {
+
+        latLabel.style.color = GREY_COLOUR;
+        lonLabel.style.color = GREY_COLOUR;
+
+        chimeButton.disabled = false;
+
+        hideLabels();
+
+    }
+
+}
+
+function loadPage () {
 
     checkMobile();
     updateTime();
+
+    locationSwitch.addEventListener('click', handleLocationToggle);
 
     chimeButton.addEventListener('click', handleChime);
 
@@ -156,14 +325,20 @@ window.addEventListener('load', () => {
 
     audioMothChimeConnector = new AudioMothChimeConnector();
 
-    timeHolder = document.getElementById('time-holder');
-
     iphoneWarning = document.getElementById('iphone-warning');
 
     timeLabel = document.getElementById('time-label');
     timeZoneLabel = document.getElementById('time-zone-label');
 
+    locationSwitchLabel = document.getElementById('location-switch-label');
+    locationSwitch = document.getElementById('location-switch');
+    latLabel = document.getElementById('lat-label');
+    lonLabel = document.getElementById('lon-label');
+    waitingLabel = document.getElementById('waiting-label');
+
     chimeButton = document.getElementById('chime-button');
+
+    locationSwitch.checked = false;
 
     // Register service worker
 
